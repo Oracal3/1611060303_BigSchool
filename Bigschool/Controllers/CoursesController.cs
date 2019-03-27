55580622e34f7576034aae3d827f5d440099831c
Quang Trung <Administrator@PC013>
2019-03-27 15:50:56 +0700

    Update first

---
+++ Bigschool/Controllers/CoursesController.cs
@@ -0,0 +1,57 @@
+﻿using Bigschool.Models;
+using Bigschool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace Bigschool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        public object ViewModel { get; private set; }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categoris.ToList()
+            };
+            return View(viewModel);
+        }
+
+        // GET: Courses
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categoris.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerID = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDatetime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index","Home");
+    }
+        
+}
+}
---
+++ Bigschool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Bigschool.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bigschool.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required(ErrorMessage = "Not be empty")]
+        public string Place { get; set; }
+
+        [Required(ErrorMessage = "Not be empty")]
+        [FutureDate]
+        public string Date { get; set; }
+
+        [Required(ErrorMessage = "Not be empty")]
+        public string Time { get; set; }
+
+        [Required(ErrorMessage = "Not be empty")]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDatetime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
