5bcb50d3dc9c1e3ed6ae665dbb32dc845bd50cf1
ngquangtrung3110998 gmail.com <ngquangtrung3110998@gmail.com>
2019-04-10 08:37:08 +0700

    final

--- Bigschool/Controllers/AttendancesController.cs
+++ Bigschool/Controllers/AttendancesController.cs
@@ -37,14 +37,16 @@ namespace Bigschool.Controllers
             return Ok();
         }
 
-        [HttpDelete]
+        
+        [HttpPost]
         public IHttpActionResult Cancel(int id)
         {
             var userId = User.Identity.GetUserId();
-            var course = _dbContext.Attendances.Single(c => c.CourseId == id && c.AttendeeId == userId);
+            var course = _dbContext.Attendances.SingleOrDefault(c => c.CourseId == id && c.AttendeeId == userId);
 
             _dbContext.Attendances.Remove(course);
             _dbContext.SaveChanges();
+            
             return Ok();
         }
     }
